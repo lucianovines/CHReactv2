82ac65c7fce60929ffeb008f30b8e066dafaeef2
Luciano Viñes <luciano_lva@hotmail.com>
2021-08-19 22:42:07 -0300

    Entrega para clase 7

---
+++ src/jsons/ProductsJson.js
@@ -0,0 +1,26 @@
+export const productsJSON =[
+    {
+        id: 1,
+        title: "Producto 1",
+        description: "Prueba de Producto 1",
+        price: 200.00,
+        imgUrl: "producto1",
+        detail: "Este es el detalle del Producto 1"
+    },
+    {
+        id: 2,
+        title: "Producto 2",
+        description: "Esta es la descripcion del producto numero 2",
+        price: 150.99,
+        imgUrl: "producto1",
+        detail: "Este es el detalle del Producto 2"
+    },
+    {
+        id: 3,
+        title: "Producto 3",
+        description: "Esta es la descripcion del producto numero 3",
+        price: 5500,
+        imgUrl: "producto1",
+        detail: "Este es el detalle del Producto 3"
+    }
+]
